092cc27553455d79b7e0dd98efeede1b1ba06a96
AhmadAlHouzani <Ahmadalhouzani@gmail.com>
2024-10-08 09:38:43 +0300

    Changed Output Into Hello World
    Edit 2: Changed class to HelloWorld

---
+++ module1/src/tr/edu/maltepe/oop/HelloWorld.java
@@ -0,0 +1,10 @@
+package tr.edu.maltepe.oop;
+public  class HelloWorld {
+
+ public static void main(String []args){
+        //
+       System.out.println("Simple Hello World by Ahmet YILMAZ (230706802)");
+        }
+}
+
+
